ea7385cb179db5e8746a94437ef232eee2e256d0
CruzInc <abstractime@outlook.com>
2024-01-24 23:20:14 -0600

    Use of JPA instead of JDBC. Use of @Entity for Course bean. Add show-sql in application.propperties for SQL to work

--- src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -5,21 +5,24 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
+import com.in28minutes.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
 
 // CommandLineRunner an interface that indicates that a bean should 
 // run when it is contained within a SpringApplication
 @Component
 public class CourseJdbcCommandLineRunner implements CommandLineRunner{
 	
+//	@Autowired
+//	private CourseJdbcRepository repository;
+
 	@Autowired
-	private CourseJdbcRepository repository;
-	
+	private CourseJpaRepository repository;
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		repository.insert(new Course(1, "Learn JDBC", "in28minutes"));
-		repository.insert(new Course(2, "Learn AWS", "in28minutes"));
-		repository.insert(new Course(3, "Learn Linux", "in28minutes"));
+		repository.insert(new Course(1, "Learn JDBC JPA", "in28minutes"));
+		repository.insert(new Course(2, "Learn AWS JPA", "in28minutes"));
+		repository.insert(new Course(3, "Learn Linux JPA", "in28minutes"));
 		
 		repository.deleteById(2);
 		
