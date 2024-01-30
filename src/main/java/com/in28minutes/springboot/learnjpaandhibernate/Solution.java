75825250875eefb3f326288db990875c244862e9
CruzInc <abstractime@outlook.com>
2024-01-30 17:46:28 -0600

    JPARepository: Use of JPA to query and retrieve Course by Author and Name.

--- src/main/java/com/in28minutes/springboot/learnjpaandhibernate/Solution.java
+++ src/main/java/com/in28minutes/springboot/learnjpaandhibernate/Solution.java
@@ -1,7 +1,26 @@
 package com.in28minutes.springboot.learnjpaandhibernate;
 
-public class Solution {
-	public long countTheNumOfKFreeSubsets(int[] nums, int k) {
-		return 0L;
-	}
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        boolean[][] visited = new boolean[mat.length][mat[0].length];
+        Queue<int[]> q = new LinkedList<>();
+        
+        boolean zeroFound = false;
+        // find 0, once found put all zeros on a queue
+        for (int i = 0; i < mat.length && zeroFound; i++) {
+            for (int j = 0; j < mat[0].length && zeroFound; j++) {
+                if (0 == mat[i][j]) {
+                    q.add(new int[]{i, j});
+                }
+            }
+        }      
+        String s = "hello";
+        
+
+
+        return mat;
+    }
 }
--- src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 
 import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
 import com.in28minutes.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
+import com.in28minutes.springboot.learnjpaandhibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 
 // CommandLineRunner an interface that indicates that a bean should 
 // run when it is contained within a SpringApplication
@@ -15,19 +16,31 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner{
 //	@Autowired
 //	private CourseJdbcRepository repository;
 
+//	@Autowired
+//	private CourseJpaRepository repository;
+	
 	@Autowired
-	private CourseJpaRepository repository;
+	private CourseSpringDataJpaRepository repository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		repository.insert(new Course(1, "Learn JDBC JPA", "in28minutes"));
-		repository.insert(new Course(2, "Learn AWS JPA", "in28minutes"));
-		repository.insert(new Course(3, "Learn Linux JPA", "in28minutes"));
+		repository.save(new Course(1, "Learn JDBC JPA", "in28minutes"));
+		repository.save(new Course(2, "Learn AWS JPA", "in28minutes"));
+		repository.save(new Course(3, "Learn Linux JPA", "in28minutes"));
+		repository.save(new Course(4, "Learn Azure JPA", "codecamp"));
+		repository.save(new Course(5, "Learn Azure JPA", "in28minutes"));
 		
-		repository.deleteById(2);
+		repository.deleteById(2l);
 		
-		System.out.println(repository.findById(1));
-		System.out.println(repository.findById(3));
+		System.out.println(repository.findById(1l));
+		System.out.println(repository.findById(3l));
+		
+		System.out.println("-------- Course Name: ");
+		System.out.println(repository.findByName("Learn Azure JPA"));
+		
+		System.out.println("-------- Author: codecamp");
+		System.out.println(repository.findByAuthor("codecamp"));
 	}
 	
 }
